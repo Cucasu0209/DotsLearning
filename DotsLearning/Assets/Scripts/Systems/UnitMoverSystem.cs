c516213c0b0420fd9f5e55207d13db56dec8d0f3
Trung nq <trung.nq24092000@gmail.com>
2025-01-05 00:28:51 +0700

    lesson 4 - Unit Move Physics

--- DotsLearning/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ DotsLearning/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -2,14 +2,29 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Physics;
+
 partial struct UnitMoverSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>>())
+        foreach ((
+            RefRW<LocalTransform> localTransform, 
+            RefRO<MoveSpeed> moveSpeed,
+            RefRW<PhysicsVelocity> physicVelocity) 
+            in SystemAPI.Query<
+                RefRW<LocalTransform>, 
+                RefRO<MoveSpeed>,
+                RefRW<PhysicsVelocity>>())
         {
-            localTransform.ValueRW.Position = localTransform.ValueRO.Position + new float3(moveSpeed.ValueRO.Value, 0, 0) * SystemAPI.Time.DeltaTime;
+            float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0);
+            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+            //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime;
+            physicVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.Value;
+            physicVelocity.ValueRW.Angular = float3.zero;
         }
     }
 }
