2e5679bd1bcffeebcda9385f2ea681f63b1682b5
Trung nq <trung.nq24092000@gmail.com>
2025-01-05 01:06:14 +0700

    Lesson 5 -Mouse World Position

---
+++ DotsLearning/Assets/Scripts/Mono/MouseWorldPosition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorldPosition : MonoBehaviour
+{
+    public static MouseWorldPosition Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public Vector3 GetPosition()
+    {
+        Ray mouseCameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+        if (plane.Raycast(mouseCameraRay, out float distance))
+        {
+            return mouseCameraRay.GetPoint(distance);
+        }
+        return Vector3.zero;
+    }
+}
--- DotsLearning/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ DotsLearning/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -10,18 +10,22 @@ partial struct UnitMoverSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         foreach ((
-            RefRW<LocalTransform> localTransform, 
+            RefRW<LocalTransform> localTransform,
             RefRO<MoveSpeed> moveSpeed,
-            RefRW<PhysicsVelocity> physicVelocity) 
+            RefRW<PhysicsVelocity> physicVelocity)
             in SystemAPI.Query<
-                RefRW<LocalTransform>, 
+                RefRW<LocalTransform>,
                 RefRO<MoveSpeed>,
                 RefRW<PhysicsVelocity>>())
         {
-            float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0);
+            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
             float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
             moveDirection = math.normalize(moveDirection);
-            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+
+            float rotationSpeed = 10f;
+
+
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(moveDirection, math.up()), SystemAPI.Time.DeltaTime * rotationSpeed);
             //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime;
             physicVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.Value;
             physicVelocity.ValueRW.Angular = float3.zero;
